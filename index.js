fbfa437fc47e930c6bc3b9e7e9c49e9f962736e5
Hieu Dang <hoanghieu3690@gmail.com>
2018-02-11 16:35:15 +0900

    create simple user model using mongoose schema

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()),
-  request = require('request');
+  request = require('request'),
+  User = require('./models/user.js');
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
